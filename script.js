859c03a9bdfca4fffd91c2a1237e9a0927c896a5
Hexmur <mauroalizzi@gmail.com>
2025-12-10 22:38:13 +0100

    implemented clear button function, updated other buttons behaviour

--- script.js
+++ script.js
@@ -34,15 +34,13 @@ let operator = '';
 const container = document.querySelector("#buttonsDiv");
 
 const buttons = container.querySelectorAll(".btn");
-const clearBtn = container.querySelector("#dispkay");
+const clearBtn = container.querySelector("#clearBtn");
 const display = container.querySelector("#display");
 
 let displayStr = display.textContent;
 let firstTime = true;
 let opPressed = false;
 
-let firstNumber = 0;
-let secondNumber = 0;
 let opertaorTyped = '';
 let firstNumberTyped = false;
 let secondNumberTyped = false;
@@ -53,7 +51,9 @@ buttons.forEach((button) => {
       firstTime = false;
       display.textContent = "";
     }
+    
     if(button.id != "opBtn" && button.textContent != "clear"){
+      if(firstNumberTyped == false) display.textContent = '';
       display.textContent += button.textContent;
       displayStr = display.textContent;
       firstNumberTyped = true;
@@ -66,12 +66,58 @@ buttons.forEach((button) => {
       displayStr = display.textContent;
       opPressed = true;
       opertaorTyped = button.textContent;
+    } else if(opPressed === true && firstNumberTyped === true
+        && button.textContent != '=' && secondNumberTyped === false){
+      console.log("heyyy");
+      display.textContent = display.textContent.substring(0, displayStr.indexOf(opertaorTyped));
+      display.textContent += button.textContent;
+      displayStr = display.textContent;
+      opertaorTyped = button.textContent;
     } else if(button.textContent === '=' && secondNumberTyped){
       let a = parseInt( displayStr.substring(0, displayStr.indexOf(opertaorTyped)) );
       let b = parseInt( displayStr.substring(displayStr.indexOf(opertaorTyped)+1, displayStr.length) );
-      console.log(a + opertaorTyped + b);
+      if(opertaorTyped === '/' && b === 0){
+        display.textContent = "I can't divide by 0!";
+        firstTime = false;
+        opPressed = false;
+        opertaorTyped = '';
+        firstNumberTyped = false;
+        secondNumberTyped = false;
+      } else{
+        console.log(a + opertaorTyped + b);
+        console.log(operate(a,b,opertaorTyped));
+        display.textContent = operate(a,b,opertaorTyped);
+        firstNumberTyped = true;
+
+        firstTime = false;
+        opPressed = false;
+
+        opertaorTyped = '';
+        secondNumberTyped = false;
+      }
+    } else if((button.id === "opBtn" && secondNumberTyped)){
+      let a = parseInt( displayStr.substring(0, displayStr.indexOf(opertaorTyped)) );
+      let b = parseInt( displayStr.substring(displayStr.indexOf(opertaorTyped)+1, displayStr.length) );
       console.log(operate(a,b,opertaorTyped));
-      display.textContent = operate(a,b,opertaorTyped);
+      display.textContent = operate(a,b,opertaorTyped) + button.textContent;
+
+      firstTime = false;
+      opPressed = true;
+
+      opertaorTyped = button.textContent;
+      firstNumberTyped = true;
+      secondNumberTyped = false;
     }
   })
+});
+
+clearBtn.addEventListener("click", () => {
+  console.log("ciaooo");
+  firstTime = true;
+  opPressed = false;
+
+  opertaorTyped = '';
+  firstNumberTyped = false;
+  secondNumberTyped = false;
+  display.textContent = '--------------------------------------';
 })
