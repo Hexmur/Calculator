aed3be293af4caf67ba263df2e5fc10a142e0511
Hexmur <mauroalizzi@gmail.com>
2025-12-10 19:33:05 +0100

    Added function for buttons pressed

--- script.js
+++ script.js
@@ -23,6 +23,30 @@ function operate(a, b, operator){
   }
 }
 
+function writeDisplay(){
+
+}
+
 let a = 0;
 let b = 0;
 let operator = '';
+
+const container = document.querySelector("#buttonsDiv");
+
+const buttons = container.querySelectorAll(".btn");
+const clearBtn = container.querySelector("#dispkay");
+const display = container.querySelector("#display");
+
+let displayStr = display.textContent;
+let firstTime = true;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if(firstTime === true){
+      firstTime = false;
+      display.textContent = "";
+    }
+    display.textContent += button.textContent;
+    displayStr = display.textContent;
+  })
+})
